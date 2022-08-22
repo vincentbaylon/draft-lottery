e55139358f9d4d078f59915c4e6589cccd804c1e
Vincent <vincentbaylon@gmail.com>
2022-08-21 18:50:27 -0700

    Design changes

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -1,61 +1,62 @@
-import Card from './Card'
-import { useEffect, useState } from 'react'
+import Card from "./Card";
+import { useEffect, useState } from "react";
 
 function Main({ list, setList }) {
-    const [randomArr, setRandomArr] = useState(list)
-    const [random, setRandom] = useState(true)
+  const [randomArr, setRandomArr] = useState(list);
+  const [random, setRandom] = useState(true);
 
-    useEffect(() => {
-        console.log('MAIN')
-    }, [randomArr])
+  useEffect(() => {
+    console.log("MAIN");
+  }, [randomArr]);
 
-    let i = 0
-    const cardContent = list.map((eachList, index) => {
-        i++
-        return <Card key={index} eachList={eachList} order={i} />
-    })
+  let i = 0;
+  const cardContent = list.map((eachList, index) => {
+    i++;
+    return <Card key={index} eachList={eachList} order={i} />;
+  });
 
-    const shuffleArr = (array) => {
-        let i = array.length - 1;
-        for (; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-        }
-        return array;
+  const shuffleArr = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
+    return array;
+  };
 
-    const shuffleArrMore = (array) => {
-        let i = array.length - 1;
-        for (; i > 0; i--) {
-          const j = Math.floor(Math.random() * (i + 1));
-          const temp = array[i];
-          array[i] = array[j];
-          array[j] = temp;
-        }
-        return array;
+  const shuffleArrMore = (array) => {
+    let i = array.length - 1;
+    for (; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
+    return array;
+  };
 
-    const handleRandom = () => {
-        const randomize = shuffleArr(list)
-        const randomizeAgain = shuffleArrMore(randomize)
-        setList(randomizeAgain)
-        setRandom(!random)
-    }
+  const handleRandom = () => {
+    const randomize = shuffleArr(list);
+    const randomizeAgain = shuffleArrMore(randomize);
+    setList(randomizeAgain);
+    setRandom(!random);
+  };
 
-    return (
-        <div className='ui two column centered grid'>
-            <br></br>
-            <div className='row'>
-                <button className='massive ui red button' onClick={handleRandom}>RANDOMIZE</button>
-            </div>
+  return (
+    <div className="ui two column centered grid">
+      <p>Created by: Vince Baylon</p>
+      <br></br>
+      <div className="row">
+        <button className="massive ui red button" onClick={handleRandom}>
+          RANDOMIZE
+        </button>
+      </div>
 
-            <div className='column'>
-                {random ? cardContent : cardContent}
-            </div>
-        </div>
-    )
+      <div className="column">{random ? cardContent : cardContent}</div>
+    </div>
+  );
 }
 
-export default Main
+export default Main;
